e968b504332daee9ff5ec4de610a0f19610e4910
Zendard <thijsmuylaert@hotmail.com>
2024-03-28 22:59:10 +0100

    initial landing/signup page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+use rocket::fs::FileServer;
+use rocket::fs::NamedFile;
+
+mod auth;
+
+#[get("/")]
+async fn index() -> Option<NamedFile> {
+    NamedFile::open("views/index.html").await.ok()
+}
+
+#[launch]
+fn rocket() -> _ {
+    let connection = sqlite::open(":memory:").unwrap();
+    let query = "CREATE TABLE users (email TEXT, password TEXT);";
+    connection.execute(query).unwrap();
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/auth", routes![auth::signup_get, auth::signup_post])
+        .mount("/public", FileServer::from("public"))
 }
