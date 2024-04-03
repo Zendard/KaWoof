43023784ac030f4cb594332c341222f74b2f9bb7
Zendard <thijsmuylaert@hotmail.com>
2024-04-03 22:20:14 +0200

    Add token generation and id db column

--- src/auth.rs
+++ src/auth.rs
@@ -1,51 +1,81 @@
+use crate::UserAuth;
+use cookie::time::Duration;
 use rocket::form::Form;
 use rocket::fs::NamedFile;
+use rocket::http::{Cookie, CookieJar};
+
+#[derive(FromForm, Debug, PartialEq)]
+pub struct UserDB {
+    id: u32,
+    email: String,
+    password: String,
+}
+#[derive(FromForm, Debug, PartialEq)]
+pub struct UserLogin {
+    email: String,
+    password: String,
+}
 
 #[get("/signup")]
 pub async fn signup_get() -> Option<NamedFile> {
     NamedFile::open("views/signup.html").await.ok()
 }
 
-#[derive(FromForm, Debug, PartialEq)]
-pub struct User {
-    email: String,
-    password: String,
-}
-
 #[post("/signup", data = "<user>")]
-pub async fn signup_post(user: Form<User>) {
+pub async fn signup_post(user: Form<UserLogin>) -> String {
     println!("New user:{:?}", user);
     let connection = rusqlite::Connection::open("./db/users.db").unwrap();
-    let query = "INSERT INTO users VALUES (:email, :password);";
-    let _ = connection.execute(query, (&user.email, &user.password));
+    let query = "INSERT INTO users(email,password) VALUES (:email, :password);";
+    let exec = connection.execute(query, (&user.email, &user.password));
+    println!("{:#?}", exec);
+    return "Tjoem".to_string();
 }
 
-async fn get_users() -> Vec<User> {
+async fn get_users() -> Vec<UserDB> {
     let connection = rusqlite::Connection::open("./db/users.db").unwrap();
-    let query = "SELECT email,password FROM users";
+    let query = "SELECT * FROM users";
     let rows = connection
         .prepare(query)
         .unwrap()
         .query_map([], |row| {
-            Ok(User {
-                email: row.get(0)?,
-                password: row.get(1)?,
+            Ok(UserDB {
+                id: row.get(0)?,
+                email: row.get(1)?,
+                password: row.get(2)?,
             })
         })
         .unwrap()
         .map(|row| row.unwrap())
         .collect();
+    println!("{:#?}", rows);
     return rows;
 }
 
-#[post("/check-user", data = "<user>")]
-pub async fn check_user(user: Form<User>) -> String {
+#[post("/check-user", data = "<user_login>")]
+pub async fn check_user(user_login: Form<UserLogin>, cookies: &CookieJar<'_>) -> Option<String> {
     let userdb = get_users().await;
-    if !userdb.contains(&user) {
-        return "Wrong password".to_string();
-    } else {
-        return "Logged in!".to_string();
+
+    let correct_user_vec: Vec<&UserDB> = userdb
+        .iter()
+        .filter(|e| e.email == user_login.email && e.password == user_login.password)
+        .collect();
+
+    if correct_user_vec.len() != 1 {
+        println!("Wrong password");
+        return None;
     }
+
+    let token = UserAuth::sign(UserAuth {
+        id: correct_user_vec[0].id,
+    });
+    println!("{:#?}", UserAuth::decode(token.clone()));
+
+    cookies.add(
+        Cookie::build(("token", token.clone()))
+            .secure(true)
+            .max_age(Duration::weeks(5)),
+    );
+    return Some(token);
 }
 
 #[get("/login")]
--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,37 @@
 extern crate rocket;
 use rocket::fs::FileServer;
 use rocket::fs::NamedFile;
-
 mod auth;
 
 #[get("/")]
-async fn index() -> Option<NamedFile> {
+async fn index(user: UserAuth) -> Option<NamedFile> {
+    println!("{:#?}", user);
     NamedFile::open("views/index.html").await.ok()
+}
+
+#[get("/", rank = 2)]
+async fn no_account() -> Option<NamedFile> {
+    NamedFile::open("views/no_account.html").await.ok()
+}
+
+static SECRET_KEY: &str = env!("SECRET_KEY");
+
+#[rocket_jwt_new::jwt(SECRET_KEY, cookie = "token")]
+pub struct UserAuth {
+    id: u32,
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index])
+        .mount("/", routes![index, no_account])
         .mount(
             "/auth",
             routes![
                 auth::signup_get,
                 auth::signup_post,
                 auth::login,
-                auth::check_user
+                auth::check_user,
             ],
         )
         .mount("/public", FileServer::from("public"))
